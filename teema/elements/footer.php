8106fff9d27a0c495dacb27f84a8b7e3bbde0634
Ari <arilankinen4@gmail.com>
2023-04-03 19:28:50 +0300

    alustus 2

--- teema/elements/footer.php
+++ teema/elements/footer.php
@@ -6,26 +6,10 @@ defined('C5_EXECUTE') or die("Access Denied.");
 <div class="footer">
   <div class="container">
     <div class="row">
-      <div class="col-lg-2 footer-first">
+      <div class="col-lg-12 footer-first">
         <?php
         // new GlobalArea('nimi') luo yleisen alueen (jos sen nimistä ei vielä ole)
         $a = new GlobalArea('Footer First');
-        $a->display();
-        ?>
-      </div>
-      <div class="col-lg-10 footer-second">
-        <?php
-        $a = new GlobalArea('Footer Second');
-        $a->display();
-        ?>
-      </div>
-    
-    </div>
-    <div class="row">
-      <div class="col-md-12 footer-bottom">
-        <?php
-        // new GlobalArea('nimi') luo yleisen alueen (jos sen nimistä ei vielä ole)
-        $a = new GlobalArea('Footer Bottom');
         $a->display();
         ?>
       </div>
